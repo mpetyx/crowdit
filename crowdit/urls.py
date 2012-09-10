25419582478ff043ca60c4a7e8b51bca0304c18c
john econ <johnnyecon@gmail.com>
2012-09-11 01:32:31 +0300

    typo fixed

--- crowdit/urls.py
+++ crowdit/urls.py
@@ -14,7 +14,7 @@ crowdit_api.register(UserSignUpResource())
 crowdit_api.register(UserResource())
 crowdit_api.register(EventResource())
 crowdit_api.register(AwardResource())
-crowdit_api.register(FriendshipInvitationResourse())
+crowdit_api.register(FriendshipInvitationResource())
 
 urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
